2e99ad162b30b380f82d25a0b6be94caa5e5b506
kreczko <lkreczko@googlemail.com>
2021-10-22 15:28:20 +0100

    initial work on storage reporting

--- src/dice_cli/admin/report/__init__.py
+++ src/dice_cli/admin/report/__init__.py
@@ -1,16 +1,27 @@
-from typing import List
-import logging
+import os
+from pathlib import Path
+from typing import List, Optional
+
 import typer
 
 from dice_cli.logger import admin_logger
-
+from dice_cli.utils import convert_to_largest_unit
 
 app = typer.Typer(help="Commands for report creation")
 
 
 @app.command()
-def storage(paths: List[str] = typer.Argument(...)) -> None:
-    admin_logger.warning("Not implemented yet")
+def storage(
+    paths: List[Path] = typer.Argument(...),
+    output_directory: Optional[Path] = typer.Option(
+        "--output", "-o", help="Output directory"
+    ),
+) -> None:
+    admin_logger.warning(":construction: Work in progress :construction:")
     for path in paths:
         admin_logger.info(f"Creating report for storage {path}")
-
+        total = sum(
+            os.stat(item).st_size for item in path.glob("**/*") if os.path.exists(item)
+        )
+        total_scaled, unit = convert_to_largest_unit(total, "B", scale=1024)
+        admin_logger.info(f"{total_scaled = :.2f} {unit}")
--- src/dice_cli/utils.py
+++ src/dice_cli/utils.py
@@ -1,5 +1,5 @@
 from itertools import count
-from typing import Iterable, Iterator
+from typing import Iterable, Iterator, Tuple
 
 
 def current_linux_user() -> str:
@@ -40,3 +40,18 @@ def groupby_range(x: int, c: Iterator[int] = count()) -> int:
 
 def groupby_inverse_range(x: int, c: Iterator[int] = count()) -> int:
     return x - next(c)
+
+
+def convert_to_largest_unit(
+    value: float, unit: str, scale: float = 1000.0
+) -> Tuple[float, str]:
+    """Converts value to largest unit of the same type"""
+    prefixes = ["", "k", "M", "G", "T", "P", "E", "Z", "Y"]
+    current_scale = value
+    prefix_index = 0
+    for i, _ in enumerate(prefixes):
+        if current_scale < scale:
+            prefix_index = i
+            break
+        current_scale /= scale
+    return current_scale, prefixes[prefix_index] + unit
