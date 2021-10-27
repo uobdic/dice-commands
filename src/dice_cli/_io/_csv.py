176d189aa598ac0bb5b8f13c74590e021d0485bd
Luke Kreczko <lkreczko@googlemail.com>
2021-10-27 14:34:46 +0100

    added new output method for List[Tuple] --> CSV

--- src/dice_cli/_io/_csv.py
+++ src/dice_cli/_io/_csv.py
@@ -1,8 +1,9 @@
 import csv
-from typing import Any, Dict, List
+from pathlib import Path
+from typing import Any, Dict, List, Union
 
 
-def read_ints_from_csv(filename: str, fieldname: str) -> List[int]:
+def read_ints_from_csv(filename: Union[str, Path], fieldname: str) -> List[int]:
     """
     Reads data from CSV file and converts {fieldname} to int.
     Returned data is sorted
@@ -15,9 +16,18 @@ def read_ints_from_csv(filename: str, fieldname: str) -> List[int]:
 
 
 def write_list_data_as_dict_to_csv(
-    data: List[Dict[str, Any]], output_file: str, fieldnames: List[str]
+    data: List[Dict[str, Any]], fieldnames: List[str], output_file: Union[str, Path]
 ) -> None:
     with open(output_file, "w") as f:
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
+        writer.writerows(data)
+
+
+def write_list_data_to_csv(
+    data: List[Any], fieldnames: List[str], output_file: Union[str, Path]
+) -> None:
+    with open(output_file, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(fieldnames)
         writer.writerows(data)
