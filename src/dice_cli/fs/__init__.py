9482449fa128889408971de65057ff5b9321b698
Luke Kreczko <lkreczko@googlemail.com>
2022-09-16 14:27:14 +0100

    added placeholder for path operations

--- src/dice_cli/fs/__init__.py
+++ src/dice_cli/fs/__init__.py
@@ -1,8 +1,23 @@
+from typing import Any
+
 import typer
+from dice_lib import load_config
 
 from ..logger import user_logger
 
 app = typer.Typer(help="DICE filesystem commands")
+
+
+def _prepare_paths(paths: list[str], config: dict[str, Any]) -> list[str]:
+    """
+    1. Remove trailing slashes from paths
+    2. lookup file system mounts
+    3. Replace protocols (e.g. /hdfs/<path> --> hdfs://<path>)
+    4. If no protocol is specified, assume local filesystem
+    """
+    if config:
+        pass
+    return [path.rstrip("/") for path in paths]
 
 
 @app.command()
@@ -20,5 +35,7 @@ def copy_from_local(
     dst : str
         Destination path.
     """
+    config = load_config()
+    src, dst = _prepare_paths([src, dst], config)
     user_logger.info(f"Copying file from local to DICE: {src} -> {dst}")
     user_logger.warning("Not implemented yet")
